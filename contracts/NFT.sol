00967a7156591cf634ced3fc73a7970940fed0e7
Yulia <86914900+yuliaye@users.noreply.github.com>
2023-01-31 14:40:46 +0800

    Initial commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Pausable, Ownable {
+    constructor() ERC721("NFT", "MTK") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    internal
+    whenNotPaused
+    override
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+}
